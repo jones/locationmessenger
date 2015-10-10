ff3fcaccb99605883c379b09cd805cc96dd8cf12
Tristan Jones <tristanjones@berkeley.edu>
2015-10-10 16:38:16 -0700

    Added geocode

--- app/src/main/java/jain/locationmessenger/MainActivity.java
+++ app/src/main/java/jain/locationmessenger/MainActivity.java
@@ -3,7 +3,9 @@ import android.app.Activity;
 import android.os.Bundle;
 
 import com.here.android.mpa.common.GeoCoordinate;
+import com.here.android.mpa.common.GeoPosition;
 import com.here.android.mpa.common.OnEngineInitListener;
+import com.here.android.mpa.common.PositioningManager;
 import com.here.android.mpa.mapping.Map;
 import com.here.android.mpa.mapping.MapFragment;
 
@@ -20,6 +22,9 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
+
         // Search for the map fragment to finish setup by calling init().
         mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mapfragment);
         mapFragment.init(new OnEngineInitListener() {
@@ -28,8 +33,15 @@ public class MainActivity extends Activity {
                 if (error == OnEngineInitListener.Error.NONE) {
                     // retrieve a reference of the map from the map fragment
                     map = mapFragment.getMap();
+
+                    PositioningManager positioningManager = PositioningManager.getInstance();
+                    positioningManager.start(PositioningManager.LocationMethod.GPS_NETWORK);
+                    GeoPosition geoPosition = positioningManager.getLastKnownPosition();
+                    final GeoCoordinate geoCoordinate = geoPosition.getCoordinate();
+
                     // Set the map center coordinate to the Vancouver region (no animation)
-                    map.setCenter(new GeoCoordinate(49.196261, -123.004773, 0.0), Map.Animation.NONE);
+                    map.setCenter(geoCoordinate, Map.Animation.LINEAR);
+                    //map.setCenter(new GeoCoordinate(49.196261, -123.004773, 0.0), Map.Animation.NONE);
                     // Set the map zoom level to the average between min and max (no animation)
                     map.setZoomLevel((map.getMaxZoomLevel() + map.getMinZoomLevel()) / 2);
                 } else {
